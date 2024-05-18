da2a7c9ccc608ac82177f534dd813e37b2dba37c
unknown <azadmilad1993@gmail.com>
2024-05-18 21:31:54 +0330

    add login page

---
+++ src/data/images.ts
@@ -0,0 +1,5 @@
+const images = {
+  loginImage: "../assets/images/Login.jpg",
+};
+
+export default images;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import styles from "../../src/styles/Login.module.scss";
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src="../../src/assets/images/logo.png" alt="Login" />
+      </div>
+
+      <form className={styles.form_container}>
+        <h1>Welcome !</h1>
+        <h3>Sign into your account</h3>
+        <input placeholder="username" name="username" />
+        <input placeholder="email" name="email" />
+        <input placeholder="password" name="password" />
+        <p>Forget password ?</p>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
